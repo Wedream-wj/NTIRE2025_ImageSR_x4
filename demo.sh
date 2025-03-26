a1687eb8b21fbcfa3a09471ba9acc1834d88a811
Wedream-wj <1749727458@qq.com>
2025-03-26 20:31:37 +0800

    fusion steps into a single Python script

--- demo.sh
+++ demo.sh
@@ -1,27 +1,4 @@
-# MambaIRv2
-python test.py --test_dir /home/wedream/mydata/srdata/DIV2K/DIV2K_test_LR_bicubic_X4_2025 \
---save_dir results \
---model_id 1
-
-# DAT, Uncomment lines 33 to 36 in the test.py file
-python test.py --test_dir /home/wedream/mydata/srdata/DIV2K/DIV2K_test_LR_bicubic_X4_2025 \
---save_dir results \
---model_id 1
-
-# HAT, Uncomment lines 39 to 42 in the test.py file
-python test.py --test_dir /home/wedream/mydata/srdata/DIV2K/DIV2K_test_LR_bicubic_X4_2025 \
---save_dir results \
---model_id 1
-
-# manual ensemble
-python ensemble_PMELSR.py
-
-# RRDB, Uncomment lines 47 to 50 in the test.py file
-python test.py --test_dir results/01_PMELSR_ensemble \
---save_dir results \
---model_id 1
-
 # all in one
 python test.py --test_dir /home/wedream/mydata/srdata/DIV2K/DIV2K_test_LR_bicubic_X4_2025_mini \
---save_dir results02 \
+--save_dir results \
 --model_id 1
